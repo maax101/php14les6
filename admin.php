10e71e36a49a58a7e59563e87e9cbbb9b1c7536b
maax101 <belous.maxim@gmail.com>
2017-09-07 10:25:02 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,21 +1,24 @@
 <?php
-if (isset ($_FILES['load_test']['name'])) {
-    $test = $_FILES['load_test']['name'];
+if (!empty($_FILES['load_test']['name'])){
+    if (isset ($_FILES['load_test']['name'])) {        
+        $test = $_FILES['load_test']['name'];
+    }
     if (file_exists($_FILES['load_test']['tmp_name'])) {
         move_uploaded_file($_FILES['load_test']['tmp_name'], 'tests/' . $test);
+    }
+    if (file_exists('tests/' . $test)) {
         echo "Файл загружен";
     } else {
-        echo "Файл НЕ загружен!";
-        exit; 
-    }   
-}
-if (isset($_FILES['load_test']['name'])) {
-    $extension = pathinfo($_FILES['load_test']['name'], PATHINFO_EXTENSION);
-    if ($extension != 'json') {
-        echo 'Выбрано неверное расширение файла';
+        echo "Файл НЕ загружен!";       
+    } 
+    if (isset($test)) {
+        $extension = pathinfo($test, PATHINFO_EXTENSION);
+        if ($extension != 'json') {
+            echo '<p><h3>Выбрано неверное расширение файла!!!</h3></p>';
+        }
     }
-}
-?>
+}  
+?> 
 <!DOCTYPE html>
 <html>
 <head>
@@ -28,6 +31,7 @@ if (isset($_FILES['load_test']['name'])) {
     <li><a href="allresult.php">Результаты</a></li>
     <br>
 </menu>
+<h4>Выберите файл *.json для загрузки:</h4>
 <form enctype="multipart/form-data" action="" method="POST">
     <input type="file" name="load_test" extensions="json"><br>
     <br>
